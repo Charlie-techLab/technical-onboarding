9490f862c9d7063b1f5b059f43631b1002b8cc97
Charlie <95556239+Charlie-techLab@users.noreply.github.com>
2023-12-21 14:44:55 -0600

    Update Footer.jsx
    Twitter link updated

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -99,7 +99,7 @@ const Footer = (props) => {
           </a>
         )}
         {twitter && (
-          <a href={`https://twitter.com/${CarlosF51105598}`} target="_blank" rel="noopener noreferrer">
+          <a href={`https://twitter.com/${https://twitter.com/CarlosF51105598}`} target="_blank" rel="noopener noreferrer">
             <img src={twitterIcon} alt="Twitter" className="socialIcon" />
           </a>
         )}
